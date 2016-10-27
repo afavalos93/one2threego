8578ed03bbcc32cabe98d2e14735d7ed38b1d38f
Andres <afavalos93@gmail.com>
2016-10-27 15:34:51 -0700

    Fixed https

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
     var messages = [];
-    var socket = io.connect('http://'+location.host);
+    var socket = io.connect('https://'+location.host);
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
